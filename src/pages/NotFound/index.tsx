8cee6947922ae9d595467e5204c24f9e8a62863d
itsmelatif <itsme.latif7@gmail.com>
2025-05-12 10:09:50 +0700

    fixing translate on not found page

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,19 +1,20 @@
 import React from "react";
 import { Box, Typography, Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
-
+import { useTranslation } from "react-i18next";
 const NotFound: React.FC = () => {
   const navigate = useNavigate();
+  const { t } = useTranslation();
   return (
     <Box textAlign="center" mt={10}>
       <Typography variant="h3" color="error" gutterBottom>
-        404 - Page Not Found
+        {t("notFoundPageTitle")}
       </Typography>
       <Typography variant="body1" mb={4}>
-        The page you're looking for doesn't exist.
+        {t("notFoundPageDescription")}
       </Typography>
       <Button variant="contained" color="primary" onClick={() => navigate("/")}>
-        Go Home
+        {t("goHome")}
       </Button>
     </Box>
   );
